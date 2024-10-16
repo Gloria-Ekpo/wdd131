c323e8553c6391d695184cd4e19326a145ef76a3
Gloria-Ekpo <104460473+Gloria-Ekpo@users.noreply.github.com>
2024-10-16 00:25:19 -0600

    cool

---
+++ week4/index.js
@@ -0,0 +1,5 @@
+const helloElement = documment.getElementById('hello')
+helloElement.innerText = 'Hello World!'
+
+const hello2Element = document.getElementById('hello2')
+hello2Element.innerText = 'Hello World'
